25e7264fb46bcf28d9d5ab7fc335d7493bc1897c
Katrin Affolter <katrin.affolter@synventis.com>
2015-02-28 20:33:27 +0100

    test 4.1

--- get-tweets1.1.php
+++ get-tweets1.1.php
@@ -16,7 +16,7 @@ function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oa
  
 $connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
  
-$tweets = $connection->get("https://api.twitter.com/1.1/trends/place.json?id=1"); //&screen_name=".$twitteruser."&count=".$notweets);
+$tweets = $connection->get("https://api.twitter.com/1.1/trends/place.json?id=23424957");
  
 echo json_encode($tweets);
 ?>
